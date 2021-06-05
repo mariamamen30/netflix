916f5a9568757d03cc298ef51d83cec79b10c777
mariam <mariam.amen30@gmail.com>
2021-06-05 17:42:30 +0200

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomeScreen from "./Screens/HomeScreen";
 import Login from "./Screens/Login";
 
 import "./App.css";
-
-const user = null;
+import { auth } from "./firebase";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
+  let user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+        // user = userAuth;
+      } else {
+        //logged out
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <Router>
